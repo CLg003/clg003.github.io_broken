45553205218ef547c9c558933a75a2c3771fe80c
CLg003 <clairemjlaing@gmail.com>
2022-05-15 09:29:51 +0100

    Fixed Projects layout.

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -4,9 +4,9 @@ import ProjectList from '../components/ProjectList';
 const Projects = () => {
 
     const projects = [
-        {name: "Project 1", type: "Project type here", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/nba_ss/pulse_home_page_ss.png"},
-        {name: "Project 2", type: "Project type here", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/nba_ss/nba_main_ss.png"},
-        {name: "Project 3", type: "Project type here", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/nba_ss/triolingo_lesson2_reset_ss.png"}
+        {name: "Pulse Gym Management", type: "Python full-stack web app", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/pulse_ss/pulse_home_page_ss.png"},
+        {name: "NBA 75", type: "React/REST API full-stack web app", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/nba_ss/nba_main_ss.png"},
+        {name: "Triolingo", type: "React full-stack web app", description: "Project description here.", image: "/assets/images/project_videos_and_screenshots/triolingo_ss/triolingo_lesson2_reset_ss.png"}
         // {name: "Project 4", type: "Project type here", description: "Project description here."},
         // {name: "Project 5", type: "Project type here", description: "Project description here."},
         // {name: "Project 6", type: "Project type here", description: "Project description here."}
